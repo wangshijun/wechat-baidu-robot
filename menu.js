45815d24dda2bfec36d6fec1419427158bbb845b
wangshijun2010 <wangshijun2010@gmail.com>
2016-06-23 07:39:52 +0800

    add menu sync script

---
+++ menu.js
@@ -0,0 +1,56 @@
+const request = require('request');
+const schedule = require('node-schedule');
+const jssdk = require('./app/libs/jssdk');
+
+request.debug = true;
+
+const menuItems = {
+    "button":[
+        {
+            "type": "click",
+            "name": "问答历史",
+            "key": "conversation-history"
+        },
+        {
+            "type": "click",
+            "name": "随机问答",
+            "key": "conversation-random"
+        }
+    ]
+};
+
+schedule.scheduleJob({ second: 0, minute: 0 }, function(){
+    console.log('about to sync menu items');
+    doMenuSync();
+});
+
+setInterval(function () {
+    console.log(new Date());
+}, 2000);
+
+function doMenuSync () {
+    jssdk.getAccessToken(function (err, token) {
+        if (err || !token) {
+            return console.error('获取 access_token 失败');
+        }
+
+        console.log({ token });
+
+        request.get(`https://api.weixin.qq.com/cgi-bin/menu/delete?access_token=${token}`, function (e, response, body) {
+            if (e) {
+                return console.error('菜单删除失败', e);
+            }
+
+            console.log('菜单删除成功', body);
+
+            request.post({ url: `https://api.weixin.qq.com/cgi-bin/menu/create?access_token=${token}`, json: menuItems }, function (_e, _response, _body) {
+                if (_e) {
+                    return console.error('菜单创建失败', _e);
+                }
+
+                console.log('菜单创建成功', body);
+            });
+        });
+    });
+};
+
