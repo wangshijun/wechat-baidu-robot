30d5b6c298f7970ab1108510758111abcf061575
wangshijun2010 <wangshijun2010@gmail.com>
2016-06-23 05:53:56 +0800

    format code

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,18 +1,18 @@
 var express = require('express'),
-  router = express.Router(),
-  mongoose = require('mongoose'),
-  Article = mongoose.model('Article');
+    router = express.Router(),
+    mongoose = require('mongoose'),
+    Article = mongoose.model('Article');
 
 module.exports = function (app) {
-  app.use('/', router);
+    app.use('/', router);
 };
 
 router.get('/', function (req, res, next) {
-  Article.find(function (err, articles) {
-    if (err) return next(err);
-    res.render('index', {
-      title: 'Generator-Express MVC',
-      articles: articles
+    Article.find(function (err, articles) {
+        if (err) return next(err);
+        res.render('index', {
+            title: 'Generator-Express MVC',
+            articles: articles
+        });
     });
-  });
 });
